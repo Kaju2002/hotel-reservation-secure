b5ca26b752df0150cc0551f5b68662dc20152111
MethmiNPCS <npcsmethmi@gmail.com>
2026-09-26 23:00:53 +0530

    Fixed the vulnerability of Account Enumeration in user routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -89,13 +89,13 @@ router.post('/login', async (req, res) => {
         // Find the user by email
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({ message: 'Invalid email' });
+            return res.status(400).json({ message: 'Invalid email or password' });
         }
 
         // Compare the provided password with the stored hashed password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid password' });
+            return res.status(400).json({ message: 'Invalid email or password' });
         }
 
         // Exclude the password from the response
